10ca81efbaebc5ec47d49ceb2bb60708f14799bf
testoccgg <testoccgg@gmail.com>
2023-08-27 21:13:08 +0300

    added include html local script

---
+++ importHTML.js
@@ -0,0 +1,24 @@
+// <script src="deferMe.js" defer></script>
+(function () {
+
+  function importHTML() {
+    let tagsRepl = document.querySelectorAll('[w3-include-HTML]');
+
+    tagsRepl.forEach(function (element) {
+       file = element.getAttribute("w3-include-html");
+      let xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+          debugger;
+          let newEl=document.createElement("div");
+          newEl.innerHTML = xhttp.responseText;
+          element.parentNode.replaceChild(newEl.firstChild, element);
+        }
+      }
+      xhttp.open("GET", file, true);
+      xhttp.send();
+    });
+  }
+
+  importHTML();
+})();
